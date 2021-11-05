72dfe71260c6a1c3d02ab06d979e1c2b86a92c80
FlorentChab <30320755+FlorentChab@users.noreply.github.com>
2021-11-05 12:13:09 +0100

    Changement fomat date des données

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var numeric = [
     ["04/09/2021","TT le", "monde au", "travail"], //mettre au format 09/04/2021 et mettre la date dans le new date 
-	["11/09/2021","Marjo & Aurel","Antoine & Flo","Kev"],
-    ["18/09/2021","Antoine & Marjo","Kev & Aurel","Flo"],
-	["25/09/2021","Kev & Flo","Antoine & Marjo","Aurel"],
-	["02/10/2021","Antoine;Aurel","Kev & Flo","Marjo"],
-	["09/10/2021","Kev & Flo","Marjo & Aurel","Antoine"],
-	["16/10/2021","Marjo & Aurel","Antoine & Flo","Kev"],
-	["23/10/2021","Antoine & Kev","Marjo & Aurel","Flo"],
-	["30/10/2021","Marjo & Flo","Antoine & Kev","Aurel"],
-	["06/11/2021","Antoine & Kev","Aurel & Flo","Marjo"],
-	["13/11/2021","Aurel & Flo","Marjo & Kev","Antoine"],
-	["20/11/2021","Marjo & Antoine","Aurel & Flo","Kev"],
-	["27/11/2021","Kev & Aurel","Marjo & Antoine","Flo"],
-	["04/12/2021","Marjo & Antoine","Kev & Flo","Aurel"],
-	["11/12/2021","Kev & Flo","Antoine;Aurel","Marjo"],
-	["18/12/2021","Marjo;Aurel","Kev & Flo","Antoine"],
-	["25/12/2021","Antoine & Flo","Marjo & Aurel","Kev"],
-	["01/01/2022","Marjo;Aurel","Antoine & Kev","Flo"],
-	["08/01/2022","Antoine & Kev","Marjo & Flo","Aurel"],
-	["15/01/2022","Aurel & Flo","Antoine & Kev","Marjo"],
-	["22/01/2022","Marjo & Kev","Aurel & Flo","Antoine"],
-	["29/01/2022","Aurel & Flo","Marjo & Antoine","Kev"],
-	["05/02/2022","Antoine & Marjo","Kev & Aurel","Flo"],
-	["12/02/2022","Kev & Flo","Antoine & Marjo","Aurel"],
-	["19/02/2022","Antoine;Aurel","Kev & Flo","Marjo"],
-	["26/02/2022","Kev & Flo","Marjo & Aurel","Antoine"],
-	["05/03/2022","Marjo & Aurel","Antoine & Flo","Kev"]
+	"["9/11/21","Marjo & Aurel","Antoine & Flo","Kev]
+	["9/18/21","Antoine & Marjo","Kev & Aurel","Flo]
+	["9/25/21","Kev & Flo","Antoine & Marjo","Aurel]
+	["10/2/21","Antoine & Aurel","Kev & Flo","Marjo]
+	["10/9/21","Kev & Flo","Marjo & Aurel","Antoine]
+	["10/16/21","Marjo & Aurel","Antoine & Flo","Kev]
+	["10/23/21","Antoine & Kev","Marjo & Aurel","Flo]
+	["10/30/21","Marjo & Flo","Antoine & Kev","Aurel]
+	["11/6/21","Antoine & Kev","Aurel & Flo","Marjo]
+	["11/13/21","Aurel & Flo","Marjo & Kev","Antoine]
+	["11/20/21","Marjo & Antoine","Aurel & Flo","Kev]
+	["11/27/21","Kev & Aurel","Marjo & Antoine","Flo]
+	["12/4/21","Marjo & Antoine","Kev & Flo","Aurel]
+	["12/11/21","Kev & Flo","Antoine & Aurel","Marjo]
+	["12/18/21","Marjo & Aurel","Kev & Flo","Antoine]
+	["12/25/21","Antoine & Flo","Marjo & Aurel","Kev]
+	["1/1/22","Marjo & Aurel","Antoine & Kev","Flo]
+	["1/8/22","Antoine & Kev","Marjo & Flo","Aurel]
+	["1/15/22","Aurel & Flo","Antoine & Kev","Marjo]
+	["1/22/22","Marjo & Kev","Aurel & Flo","Antoine]
+	["1/29/22","Aurel & Flo","Marjo & Antoine","Kev]
+	["2/5/22","Antoine & Marjo","Kev & Aurel","Flo]
+	["2/12/22","Kev & Flo","Antoine & Marjo","Aurel]
+	["2/19/22","Antoine & Aurel","Kev & Flo","Marjo]
+	["2/26/22","Kev & Flo","Marjo & Aurel","Antoine]
+	["3/5/22","Marjo & Aurel","Antoine & Flo","Kev] //nouvelle serie
 ];
 
 var i = 0;
