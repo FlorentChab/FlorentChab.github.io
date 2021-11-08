5f0983e4c5fea8f5e98d1744fad39898a87a8e30
FlorentChab <30320755+FlorentChab@users.noreply.github.com>
2021-11-08 11:18:30 +0100

    Correction des liens entre scripts et html
    + dates de distribution

--- amap_script.js
+++ amap_script.js
@@ -1,37 +1,35 @@
 var numeric = [
-	["11/11/2021","OUI","NON","?","?","?","?"]
-	["18/11/2021","OUI","OUI","?","?","?","?"]
-	["25/11/2021","OUI","NON","?","?","?","?"]
-	["02/12/2021","OUI","OUI","?","?","?","?"]
-	["09/12/2021","OUI","NON","?","?","?","?"]
-	["16/12/2021","OUI","OUI","?","?","?","?"]
-	["23/12/2021","OUI","NON","?","?","?","?"]
-	["30/12/2021","OUI","OUI","?","?","?","?"]
-	["06/01/2022","OUI","NON","?","?","?","?"]
-	["13/01/2022","OUI","OUI","?","?","?","?"]
-	["20/01/2022","OUI","NON","?","?","?","?"]
-	["27/01/2022","OUI","OUI","?","?","?","?"]
-	["03/02/2022","OUI","NON","?","?","?","?"]
-	["10/02/2022","OUI","OUI","?","?","?","?"]
-	["17/02/2022","OUI","NON","?","?","?","?"]
-	["24/02/2022","OUI","OUI","?","?","?","?"]
-	["03/03/2022","OUI","NON","?","?","?","?"]
-	["10/03/2022","OUI","OUI","?","?","?","?"]
-	["17/03/2022","OUI","NON","?","?","?","?"]
-	["24/03/2022","OUI","OUI","?","?","?","?"]
-	["31/03/2022","OUI","NON","?","?","?","?"]
-	["07/04/2022","OUI","OUI","?","?","?","?"]
-	["14/04/2022","OUI","NON","?","?","?","?"]
-	["21/04/2022","OUI","OUI","?","?","?","?"]
-	["28/04/2022","OUI","NON","?","?","?","?"]
-	["05/05/2022","OUI","OUI","?","?","?","?"]
-	["12/05/2022","OUI","NON","?","?","?","?"]
-	["19/05/2022","OUI","OUI","?","?","?","?"]
-	["26/05/2022","OUI","NON","?","?","?","?"]
-	["02/06/2022","OUI","OUI","?","?","?","?"]
+	["11/11/21","OUI","NON","OUI","?","?","?"],
+	["18/11/2021","OUI","OUI","NON","?","?","?"],
+	["25/11/2021","OUI","NON","OUI","?","?","?"],
+	["02/12/2021","OUI","OUI","NON","?","?","?"],
+	["09/12/2021","OUI","NON","OUI","?","?","?"],
+	["16/12/2021","OUI","OUI","NON","?","?","?"],
+	["23/12/2021","OUI","NON","OUI","?","?","?"],
+	["30/12/2021","OUI","OUI","NON","?","?","?"],
+	["06/01/2022","OUI","NON","OUI","?","?","?"],
+	["13/01/2022","OUI","OUI","NON","?","?","?"],
+	["20/01/2022","OUI","NON","OUI","?","?","?"],
+	["27/01/2022","OUI","OUI","NON","?","?","?"],
+	["03/02/2022","OUI","NON","OUI","?","?","?"],
+	["10/02/2022","OUI","OUI","NON","?","?","?"],
+	["17/02/2022","OUI","NON","OUI","?","?","?"],
+	["24/02/2022","OUI","OUI","NON","?","?","?"],
+	["03/03/2022","OUI","NON","OUI","?","?","?"],
+	["10/03/2022","OUI","OUI","NON","?","?","?"],
+	["17/03/2022","OUI","NON","OUI","?","?","?"],
+	["24/03/2022","OUI","OUI","NON","?","?","?"],
+	["31/03/2022","OUI","NON","OUI","?","?","?"],
+	["07/04/2022","OUI","OUI","NON","?","?","?"],
+	["14/04/2022","OUI","NON","OUI","?","?","?"],
+	["21/04/2022","OUI","OUI","NON","?","?","?"],
+	["28/04/2022","OUI","NON","OUI","?","?","?"],
+	["05/05/2022","OUI","OUI","NON","?","?","?"],
+	["12/05/2022","OUI","NON","OUI","?","?","?"],
+	["19/05/2022","OUI","OUI","NON","?","?","?"],
+	["26/05/2022","OUI","NON","OUI","?","?","?"],
+	["02/06/2022","OUI","OUI","NON","?","?","?"]
 ];
-
-//To be modified : date à checker uniquement sur la semaine à venir pas la passée (min <-4 ==> inutile)
 
 var i = 0;
 var today = new Date();
@@ -42,12 +40,12 @@ for (i; i < numeric.length; i++) {
 	vDate = new Date(numeric[i][0]);
 	
 	if (vDate > today){
-		document.getElementById('kpain').innerHTML = numeric[i][1];
-		document.getElementById('koeuf').innerHTML = numeric[i][2];
-		document.getElementById('kfromage').innerHTML = numeric[i][3];
-		document.getElementById('ktruite').innerHTML = numeric[i][4];
-		document.getElementById('kvolaille').innerHTML = numeric[i][2]
+		document.getElementById('kdate').innerHTML = "Jeudi :" + numeric[i][0];
+		document.getElementById('kpain').innerHTML = numeric[i][2];
+		document.getElementById('koeuf').innerHTML = numeric[i][3];
+		document.getElementById('kfromage').innerHTML = numeric[i][4];
+		document.getElementById('ktruite').innerHTML = numeric[i][5];
+		document.getElementById('kvolaille').innerHTML = numeric[i][6]
 		i = numeric.length;
 	}
 }
-
