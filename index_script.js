fb130c848b6df8285a12b969d2423224f65015c4
FlorentChab <30320755+FlorentChab@users.noreply.github.com>
2022-06-27 12:12:08 +0200

    Aurel to marjolaine

--- index_script.js
+++ index_script.js
@@ -1,31 +1,31 @@
 var numeric = [
-	["4/2/22","Antoine & Kev","Aurel & Flo","Marjo"], //same
-	["4/9/22","Antoine & Marjo","Kev & Flo","Aurel"],
-	["4/16/22","Kev & Aurel","Marjo & Antoine","Flo"],
-	["4/23/22","Marjo & Antoine","Aurel & Flo","Kev"],
-	["4/30/22","Aurel & Flo","Marjo & Kev","Antoine"],
-	["5/7/22","Antoine & Kev","Aurel & Flo","Marjo"],
-	["5/14/22","Marjo & Flo","Antoine & Kev","Aurel"],
-	["5/21/22","Antoine & Kev","Marjo & Aurel","Flo"],
-	["5/28/22","Marjo & Aurel","Antoine & Flo","Kev"],
-	["6/4/22","Kev & Flo","Marjo & Aurel","Antoine"],
-	["6/11/22","Antoine & Aurel","Kev & Flo","Marjo"],
-	["6/18/22","Kev & Flo","Antoine & Marjo","Aurel"],
-	["6/25/22","Antoine & Marjo","Kev & Aurel","Flo"],
-	["7/2/22","Aurel & Flo","Antoine & Marjo","Kev"],
-	["7/9/22","Marjo & Kev","Aurel & Flo","Antoine"],
-	["7/16/22","Aurel & Flo","Antoine & Kev","Marjo"],
-	["7/23/22","Antoine & Kev","Marjo & Flo","Aurel"],
-	["7/30/22","Marjo & Aurel","Antoine & Kev","Flo"],
-	["8/6/22","Antoine & Flo","Marjo & Aurel","Kev"],
-	["8/13/22","Marjo & Aurel","Kev & Flo","Antoine"],
-	["8/20/22","Kev & Flo","Antoine & Aurel","Marjo"], //same
-	["8/27/22","Antoine & Marjo","Kev & Flo","Aurel"],
-	["9/3/22","Kev & Aurel","Antoine & Marjo","Flo"],
-	["9/10/22","Antoine & Marjo","Aurel & Flo","Kev"],
-	["9/17/22","Aurel & Flo","Marjo & Kev","Antoine"],
-	["9/24/22","Antoine & Kev","Aurel & Flo","Marjo"],
-	["10/1/22","Marjo & Flo","Antoine & Kev","Aurel"],
+	["4/2/22","Antoine & Kev","MrjLaine & Flo","MrjRie"], //same
+	["4/9/22","Antoine & MrjRie","Kev & Flo","MrjLaine"],
+	["4/16/22","Kev & MrjLaine","MrjRie & Antoine","Flo"],
+	["4/23/22","MrjRie & Antoine","MrjLaine & Flo","Kev"],
+	["4/30/22","MrjLaine & Flo","MrjRie & Kev","Antoine"],
+	["5/7/22","Antoine & Kev","MrjLaine & Flo","MrjRie"],
+	["5/14/22","MrjRie & Flo","Antoine & Kev","MrjLaine"],
+	["5/21/22","Antoine & Kev","MrjRie & MrjLaine","Flo"],
+	["5/28/22","MrjRie & MrjLaine","Antoine & Flo","Kev"],
+	["6/4/22","Kev & Flo","MrjRie & MrjLaine","Antoine"],
+	["6/11/22","Antoine & MrjLaine","Kev & Flo","MrjRie"],
+	["6/18/22","Kev & Flo","Antoine & MrjRie","MrjLaine"],
+	["6/25/22","Antoine & MrjRie","Kev & MrjLaine","Flo"],
+	["7/2/22","MrjLaine & Flo","Antoine & MrjRie","Kev"],
+	["7/9/22","MrjRie & Kev","MrjLaine & Flo","Antoine"],
+	["7/16/22","MrjLaine & Flo","Antoine & Kev","MrjRie"],
+	["7/23/22","Antoine & Kev","MrjRie & Flo","MrjLaine"],
+	["7/30/22","MrjRie & MrjLaine","Antoine & Kev","Flo"],
+	["8/6/22","Antoine & Flo","MrjRie & MrjLaine","Kev"],
+	["8/13/22","MrjRie & MrjLaine","Kev & Flo","Antoine"],
+	["8/20/22","Kev & Flo","Antoine & MrjLaine","MrjRie"], //same
+	["8/27/22","Antoine & MrjRie","Kev & Flo","MrjLaine"],
+	["9/3/22","Kev & MrjLaine","Antoine & MrjRie","Flo"],
+	["9/10/22","Antoine & MrjRie","MrjLaine & Flo","Kev"],
+	["9/17/22","MrjLaine & Flo","MrjRie & Kev","Antoine"],
+	["9/24/22","Antoine & Kev","MrjLaine & Flo","MrjRie"],
+	["10/1/22","MrjRie & Flo","Antoine & Kev","MrjLaine"],
 	["10/8/22","Script à","completer","Sheh !"]
 ];
 
@@ -60,4 +60,3 @@ for (i; i < numeric.length; i++) {
 		i = numeric.length;
 	}
 }
-
